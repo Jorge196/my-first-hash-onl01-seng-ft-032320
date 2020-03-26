86256b2c9c87732d2eee886082a7842861569a7d
Jorge Ramirez <joramirez96@gmail.com>
2020-03-26 23:12:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,8 +28,14 @@ def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
   shipping_manifest["gun poweder"] = 4
-
   shipping_manifest["muskets"] = 2
+
+  puts shipping manifest 
+    {"whale bone corsets" => 5,
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+    "gun powder" => 4
+    "muskets" => 2}
 
 
   # add 2 muskets to the shipping_manifest hash below

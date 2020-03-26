0c1ee0d2cb2d5df6939799484d08995b150f4ad2
Jorge Ramirez <joramirez96@gmail.com>
2020-03-26 22:43:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+hash = {"key 1" => "whale bone corsets", "key 2" => "porcelain vases", "key 3" => "oil paintings" }
 end
 
 def adding
@@ -33,9 +33,11 @@ def adding
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3
-                 }
+                "muskets" => 2
+              "gun powder" => 4
+             }
   # add 2 muskets to the shipping_manifest hash below
-
+  
 
 
   # add 4 gun powder to the shipping_manifest hash below
